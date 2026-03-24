3c30c4f25709fcb4e7308937d59fa90470544fb1
Gabriel Santana Santos <gabrielsantanasantos798@gmail.com>
2026-03-24 14:54:21 -0300

    feat: adde initial page styles

--- app/index.tsx
+++ app/index.tsx
@@ -1,16 +1,31 @@
 import { router } from "expo-router";
-import { Text, TouchableOpacity, View } from "react-native";
+import { Image, Text, TouchableOpacity, View } from "react-native";
 
 export default function Index() {
   return (
-    <View className="flex-1 items-center justify-center">
-      <Text className="text-red-600">Bem vindo</Text>
-      <TouchableOpacity onPress={() => router.push("/login")}>
-        <Text>Login</Text>
-      </TouchableOpacity>
-      <TouchableOpacity onPress={() => router.push("/signup")}>
-        <Text>Signup</Text>
-      </TouchableOpacity>
+    <View className="flex-1 items-center justify-center gap-5">
+      <Image
+        source={require("@/assets/images/logo.png")}
+        className="w-430 h-932 mb-4"
+      />
+      <View className="items-center gap-2">
+        <Text className="font-bold text-xl">Bem vindo a minha vez</Text>
+        <Text className="text-sm">seu app de gestão de consultas</Text>
+      </View>
+      <View className="gap-4 w-[80%]">
+        <TouchableOpacity
+          className="bg-button-primary py-3 px-2 rounded-xl items-center"
+          onPress={() => router.push("/login")}
+        >
+          <Text className="text-textPrimary">Login</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          className="bg-button-primary py-3 px-2 rounded-xl items-center"
+          onPress={() => router.push("/signup")}
+        >
+          <Text className="text-textPrimary">Signup</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
