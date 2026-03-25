d430d12ecc54412ff16c640f2945194d32b8bd68
Gabriel Santana Santos <gabrielsantanasantos798@gmail.com>
2026-03-25 15:35:18 -0300

    feat: adapted to web

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const httpClient = axios.create({
-  baseURL: "http://10.0.2.2:3001", //android
-  //baseURL: "http://localhost:3001" //web and IOS
+  // baseURL: "http://10.0.2.2:3001", //android
+  baseURL: "http://localhost:3001", //web and IOS
 });
