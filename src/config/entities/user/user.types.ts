6e9122d5eba5b3d02f5cf1d29fe85084f30fd146
Gabriel Santana Santos <gabrielsantanasantos798@gmail.com>
2026-03-25 15:34:13 -0300

    feat: create user types

---
+++ src/config/entities/user/user.types.ts
@@ -0,0 +1,13 @@
+export interface IUser {
+  _id: string;
+  name: string;
+  email: string;
+  role?: EUserRole;
+  active?: boolean;
+  createdAt: Date;
+}
+
+export enum EUserRole {
+  ADMIN = "ADMIN",
+  USER = "USER",
+}
