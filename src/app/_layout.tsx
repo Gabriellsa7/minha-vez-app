ec39b1b95d1e51df757a1f548df3d86870ad11f2
Gabriel Santana Santos <gabrielsantanasantos798@gmail.com>
2026-03-25 10:26:44 -0300

    feat: add login on root layout

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -11,6 +11,7 @@ export default function RootLayout() {
     <QueryClientProvider client={queryClient}>
       <SafeAreaProvider style={{ flex: 1 }}>
         <Stack screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="login" />
           <Stack.Screen name="(tabs)" />
         </Stack>
       </SafeAreaProvider>
