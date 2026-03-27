fa820c8e9299161958916c1f42e8a428e7a76876
Gabriel Santana Santos <gabrielsantanasantos798@gmail.com>
2026-03-27 09:44:19 -0300

    feat: add new base url

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,5 +2,6 @@ import axios from "axios";
 
 export const httpClient = axios.create({
   // baseURL: "http://10.0.2.2:3001", //android
-  baseURL: "http://localhost:3001", //web and IOS
+  baseURL: "http://192.168.0.16:3001", //expo
+  // baseURL: "http://localhost:3001", //web and IOS
 });
