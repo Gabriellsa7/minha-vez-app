6e58f39662c1871f3c101860fc1c24841a804bfc
Gabriel Santana Santos <gabrielsantanasantos798@gmail.com>
2026-03-30 14:35:12 -0300

    feat: create appointment types

---
+++ src/config/entities/appointments/appointments.types.ts
@@ -0,0 +1,21 @@
+export interface IAppointment {
+  _id: string;
+  patientId: string;
+  professionalId: string;
+  healthUnitId: string;
+  queueItemId: string;
+  dateTime: Date;
+  status: EAppointmentStatus;
+  notes?: string;
+  checkInAt?: Date | null;
+  finishedAt?: Date | null;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+export enum EAppointmentStatus {
+  SCHEDULED = "SCHEDULED",
+  IN_PROGRESS = "IN_PROGRESS",
+  COMPLETED = "COMPLETED",
+  CANCELED = "CANCELED",
+}
