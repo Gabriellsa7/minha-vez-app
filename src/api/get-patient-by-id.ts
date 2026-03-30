6be7a2617a182590179ba33db5b24ca90c3f3af8
Gabriel Santana Santos <gabrielsantanasantos798@gmail.com>
2026-03-30 14:35:00 -0300

    feat: create api call to get patient by id

---
+++ src/api/get-patient-by-id.ts
@@ -0,0 +1,28 @@
+import { IPatient } from "../config/entities/patients/patients.type";
+import { generateReactQuery } from "../helpers/react-query";
+import { httpClient } from "../services/api";
+
+export const GET_PATIENT_BY_ID_KEY = "GET_PATIENT_BY_ID_KEY";
+
+export interface IGetPatientByIdParams {
+  userId: string;
+}
+
+const getPatientById = async (
+  params: IGetPatientByIdParams,
+): Promise<IPatient> => {
+  const path = `/patients/${params.userId}`;
+
+  try {
+    const response = await httpClient.get(path);
+
+    return response.data;
+  } catch (error: any) {
+    throw error;
+  }
+};
+
+export const useGetPatientById = generateReactQuery<
+  IPatient,
+  IGetPatientByIdParams
+>(GET_PATIENT_BY_ID_KEY, getPatientById);
