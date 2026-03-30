1aa76eab36d8850320fecdd47b1d26f9a9d9892a
Gabriel Santana Santos <gabrielsantanasantos798@gmail.com>
2026-03-30 14:35:23 -0300

    feat: create paitients type

---
+++ src/config/entities/patients/patients.type.ts
@@ -0,0 +1,17 @@
+export interface IPatient {
+  _id: string;
+  userId: string;
+  cpf: string;
+  birthDate: string;
+  priority: EPatientPriority;
+  phone: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+export enum EPatientPriority {
+  NORMAL = "NORMAL",
+  ELDERLY = "ELDERLY",
+  PREGNANT = "PREGNANT",
+  DISABLED = "DISABLED",
+}
