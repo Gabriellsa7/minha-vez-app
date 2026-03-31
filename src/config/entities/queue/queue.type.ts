5a6c2bd37e76586ec11acb206d63319cf90eac3f
Gabriel Santana Santos <gabrielsantanasantos798@gmail.com>
2026-03-31 15:39:19 -0300

    feat: create queue types

---
+++ src/config/entities/queue/queue.type.ts
@@ -0,0 +1,22 @@
+export interface IQueue {
+  _id: string;
+  professionalId: string;
+  healthUnitId: string;
+  status: EQueueStatus;
+  openedAt?: Date | null;
+  closedAt?: Date | null;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+export enum EQueueStatus {
+  OPEN = "OPEN",
+  IN_PROGRESS = "IN_PROGRESS",
+  CLOSED = "CLOSED",
+}
+
+export interface IQueueWithDetails {
+  _id: string;
+  healthUnitName: string;
+  queueSize: number;
+}
