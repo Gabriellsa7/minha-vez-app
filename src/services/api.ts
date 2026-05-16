0d96af1554523510f8435cacd7832211c08e0dfd
Gabriellsa7 <gabrielsantanasantos798@gmail.com>
2026-05-16 08:44:00 -0300

    feat: some changes

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,6 +2,6 @@ import axios from "axios";
 
 export const httpClient = axios.create({
   // baseURL: "http://10.0.2.2:3001", //android
-  baseURL: "http://192.168.0.16:3001", //expo
+  baseURL: "http://192.168.0.13:3001", //expo
   // baseURL: "http://localhost:3001", //web and IOS
 });
