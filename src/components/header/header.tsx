2839228ccca9a33abb4d86e3bb38dbb6b22df63e
Gabriellsa7 <gabrielsantanasantos798@gmail.com>
2026-07-19 10:47:31 -0300

    feat: add back navigation to header

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,7 +1,7 @@
 import { IUser } from "@/src/config/entities/user/user.types";
+import { router } from "expo-router";
 import { ArrowLeft, User } from "lucide-react-native";
-import { Text, View } from "react-native";
-
+import { Pressable, Text, View } from "react-native";
 interface HeaderProps {
   text: string;
   user: IUser;
@@ -11,7 +11,9 @@ export default function Header({ text, user }: HeaderProps) {
   return (
     <View className="flex-row justify-between items-center gap-4 p-4">
       <View className="flex-row items-center gap-4 p-6">
-        <ArrowLeft size={20} color="#006673" />
+        <Pressable onPress={() => router.back()}>
+          <ArrowLeft size={20} color="#006673" />
+        </Pressable>
         <Text className="text-textSecondary text-lg">{text}</Text>
       </View>
 
