6645af0d5e329b2b44b968b0abb727e70904dbc9
Gabriel Santana <gabrielsantanasantos798@gmail.com>
2026-07-20 11:50:01 -0300

    refactor: add user initials and remove info icon

--- src/features/main-content/componentes/header/header.tsx
+++ src/features/main-content/componentes/header/header.tsx
@@ -1,5 +1,6 @@
 import { IUser } from "@/src/config/entities/user/user.types";
-import { Bell, Info, User } from "lucide-react-native";
+import { getUserInitials } from "@/src/utils/util";
+import { Bell } from "lucide-react-native";
 import { Text, View } from "react-native";
 
 interface HomeHeaderProps {
@@ -7,13 +8,18 @@ interface HomeHeaderProps {
 }
 
 export default function HomeHeader({ user }: HomeHeaderProps) {
+  const userInitials = user ? getUserInitials(user.name) : "";
   return (
     <View className="flex-row items-center justify-between pb-2">
       <View className="gap-3 flex-row items-center">
-        {/* User Profile Image */}
-        <User size={32} color="#FFFFFF" />
+        <View className="w-10 h-10 rounded-full bg-bgSecondary flex items-center justify-center">
+          {/*User image profile, add a button to edit the image too*/}
+          <Text className="text-textPrimary text-lg font-bold">
+            {userInitials}
+          </Text>
+        </View>
         <View>
-          <Text className="text-textPrimary font-bold text-xl">
+          <Text className="text-textPrimary font-bold text-lg">
             Bom dia, {user?.name || "User"}
           </Text>
           <Text className="text-textThird">Permita-nós ajuda-lo</Text>
@@ -22,9 +28,6 @@ export default function HomeHeader({ user }: HomeHeaderProps) {
       <View className="gap-3 flex-row">
         <View className="rounded-full bg-bgSecondary p-2">
           <Bell size={24} color="#FFFFFF" />
-        </View>
-        <View className="rounded-full bg-bgSecondary p-2">
-          <Info size={24} color="#FFFFFF" />
         </View>
       </View>
     </View>
