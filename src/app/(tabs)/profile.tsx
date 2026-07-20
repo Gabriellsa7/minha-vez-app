6f3439a15045e6e2ba73ab2e14892248d7ae9ade
Gabriel Santana <gabrielsantanasantos798@gmail.com>
2026-07-20 11:37:15 -0300

    feat: add profile content component

--- src/app/(tabs)/profile.tsx
+++ src/app/(tabs)/profile.tsx
@@ -1,5 +1,10 @@
-import { Text } from "react-native";
+import { ProfileContent } from "@/src/features/profile-content/profile-content";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Profile() {
-  return <Text>Profile</Text>;
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <ProfileContent />
+    </SafeAreaView>
+  );
 }
