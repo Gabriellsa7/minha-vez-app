e8080e5106d8de763831446494db2f5f5a6ed4e6
Gabriel Santana <gabrielsantanasantos798@gmail.com>
2026-07-20 11:47:03 -0300

    refactor: add user to header

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,23 +1,29 @@
-import { IUser } from "@/src/config/entities/user/user.types";
+import { useGetUser } from "@/src/api/get-user-me";
+import { getUserInitials } from "@/src/utils/util";
 import { router } from "expo-router";
-import { ArrowLeft, User } from "lucide-react-native";
+import { ArrowLeft } from "lucide-react-native";
 import { Pressable, Text, View } from "react-native";
 interface HeaderProps {
   text: string;
-  user?: IUser;
 }
 
-export default function Header({ text, user }: HeaderProps) {
+export default function Header({ text }: HeaderProps) {
+  const { data: user } = useGetUser();
+  const userInitials = user ? getUserInitials(user.name) : "";
   return (
-    <View className="flex-row justify-between items-center gap-4 p-6 bg-bgThird">
+    <View className="flex-row justify-between items-center p-4 bg-bgThird">
       <View className="flex-row items-center gap-4">
         <Pressable onPress={() => router.back()}>
           <ArrowLeft size={26} color="#006673" />
         </Pressable>
         <Text className="text-textSecondary text-lg font-bold">{text}</Text>
       </View>
-      {/* User Profile Image */}
-      <User size={26} color="#006673" />
+      <View className="w-10 h-10 rounded-full bg-bgSecondary flex items-center justify-center">
+        {/*User image profile, add a button to edit the image too*/}
+        <Text className="text-textPrimary text-lg font-bold">
+          {userInitials}
+        </Text>
+      </View>
     </View>
   );
 }
