eb953e7ff079eec0b7f3647ad833deec64c0964d
Gabriel Santana <gabrielsantanasantos798@gmail.com>
2026-08-10 11:25:37 -0300

    style: change input bg

--- src/components/search-input/search-input.tsx
+++ src/components/search-input/search-input.tsx
@@ -13,7 +13,7 @@ export default function SearchInput({
   onChangeText,
 }: SearchInputProps) {
   return (
-    <View className="flex-row items-center gap-2 bg-bgPrimary py-3 px-5 rounded-2xl">
+    <View className="flex-row items-center gap-2 bg-bgThird py-3 px-5 rounded-2xl">
       <Search size={18} color="#888" />
       <TextInput
         placeholder={placeholder}
